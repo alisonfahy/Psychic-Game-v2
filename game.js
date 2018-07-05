aba270621a194d61219f7ef39baca36b2c654364
Alison Fahy <fahyalison@gmail.com>
2018-07-04 19:45:33 -0500

    update

--- game.js
+++ game.js
@@ -1,69 +1,60 @@
-// Create your global variables here
+$(document).ready(function() {
+
+//Set up variables:
 var wins = 0;
 var losses = 0;
 var guessAmt = 9;
 var lettersGuessed = [];
+
+//Attaches variables to the DOM:
+$("#Wins").text(wins);
+$("#losses").text(losses);
+$("#guess-amt").text(guessAmt);
+$("#letters-guessed").text(lettersGuessed);
+
+
+//Computer picks letter at random:
 var letters = ["a", "b", "c", "d", "e","f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 
                 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var random = Math.floor((Math.random() * letters.length - 1) + 1);
 var computerChoice = letters[random];
-
 console.log(computerChoice)
 
-// Show the initals values on the DOM - each attaches to an id in the HTML
-
-document.getElementById("Wins").innerHTML = wins;
-document.getElementById("losses").innerHTML = losses;
-document.getElementById("guess-amt").innerHTML = guessAmt;
-
-// capture key press from the user
-document.onkeypress = function(event) {
-  // The key property on the event object is used to capture which key the user presses
-  var userInput = event. key;
-//   console.log(userInput)
-  // An if...else statement is needed to compare the user's input the randomly choosen letter by the computer
-  // The condition can only evaluate to true or false
-  if (userInput === computerChoice) {
-    // This block of code runs if the condition is true
-    
-    wins++; // Shorthand for wins = wins + 1; OR wins += 1;
-    document.getElementById("Wins").innerHTML = wins;
-    }
-    
-    // if (userInput === computerChoice) 
-    //  computer generates new answer
-    
-
-    else {
-    // This block of code runs if the condition is false
-
-    // Addes the letter to the end of the array
-    lettersGuessed.push(userInput);
-
-    // Joins the array into a string and shows that on the DOM
-    document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(", ");
-
-    
-      } 
-
-    if (userInput !== computerChoice) {
-        // This block of code runs if the condition is false
-        
-        losses++; // Shorthand for losses = losses + 1; OR wins += 1;
-        document.getElementById("losses").innerHTML = losses;
-      } 
-    // Keeps the counter from going under 0
-    if (guessAmt === 0) {
-      losses++; 
-      document.getElementById("losses").innerHTML = losses;
-
-    } 
-    else if (guessAmt > 0) {
-      guessAmt--; // Shorthand for guessAmt = guessAmt - 1; OR losses -= 1;
-      document.getElementById("guess-amt").innerHTML = guessAmt;
-    }
-    if (guessAmt === 0) {
-    document.location.href = ""
-    }
-  
+//game reset function:
+function resetGame() {
+  computerChoice = letters[random];
+  guessAmt = 9;
+  $("#guess-amt").text(guessAmt);
+  lettersGuessed = [];
+  $("#letters-guessed").text(lettersGuessed);
 }
+
+//User presses key
+var userInput = $(document).keypress (function(event) {
+   userInput = event. key;
+   
+  console.log(userInput);
+
+  lettersGuessed.push(userInput);
+  $("#letters-guessed").append((userInput) + ", ");
+  
+  guessAmt--;
+  $("#guess-amt").text(guessAmt);
+
+//If guess is correct:
+if (userInput === computerChoice) {
+    wins++;
+    $("#Wins").text(wins);
+    resetGame();
+    console.log(wins);
+}
+
+//When guessAmt=0
+if (guessAmt === 0) {
+    losses++;
+    $("#losses").text(losses);
+    resetGame()
+    console.log(losses);
+}
+});
+})
